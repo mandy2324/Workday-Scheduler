e61b1de4097c3ad9577be24ed3c21777007ea13c
unknown <jasdeepsaini30@gmail.com>
2021-09-03 14:41:34 -0400

    adding all changes

---
+++ Assets/js/script.js
@@ -0,0 +1,15 @@
+// // var time = moment().format('dddd, MMMM Do');
+// // $("#currentDay").text(time);
+
+// // console.log(moment);
+
+// var today = moment();
+// $("#currentDay").text(today.format('dddd, MMMM Do'));
+
+// $("#timeHere").html(moment().format('dddd, MMMM Do'))
+
+// $(window).load(function() {
+//     var today = moment();
+//     $("#currentDay").text(today.format('dddd, MMMM Do'));
+//     console.log(moment());
+// });
