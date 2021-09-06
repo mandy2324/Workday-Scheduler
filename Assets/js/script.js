227fc41eb5202b90a1612ac1bdee82f576d5f25e
unknown <jasdeepsaini30@gmail.com>
2021-09-06 10:47:49 -0400

    adding inital func for display function in js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,26 @@
-$(function() {
-    var today = moment();
-    $("#currentDay").text(today.format("dddd, MMMM Do"));
+$(function displayCurrentDate() {
+    var currentTime = moment();
+    $("#currentDay").text(currentTime.format("dddd, MMMM Do"));
 });
+
+// setting a function to execute when page loads
+window.onload = function() {
+    var currentTimeBlocks = getCurrentTimeBlocks();
+    var currentTime = moment();
+
+    displayTimeBlockRows(currentTime);
+    displayCurrentDate(currentTime);
+
+
+    document.querySelector('.container')
+        .addEventListener('click', function(event) {
+            containerClicked(event, currentTimeBlocks);
+        })
+    setTimeBlockText(currentTime);
+}
+
+function getCurrentTimeBlocks() {
+    var currentTimeBlocks = localStorage.getItem('timeblockobjects');
+    return currentTimeBlocks ? JSON.parse(currentTimeBlocks) : [];
+}
+// function for timeblock row display
